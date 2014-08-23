1cd2826f5ceaee9cc01e9c78b245ca4c56091e51
YuLun Shih <shih@yulun.me>
2014-08-24 01:08:28 +0800

    Add getIp()

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var API_TOKEN = process.env.API_TOKEN;
 var IP = process.env.IP || "localhost";
 var PORT = process.env.PORT || 7777;
 
+function getIp(req) {
+  return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+}
+
 if (!API_TOKEN) {
   console.error("API_TOKEN=your_token_here node %s", __filename);
   console.error("https://developer.uber.com/v1/tutorials/#server-side-authentication");
@@ -14,7 +21,7 @@ if (!API_TOKEN) {
 }
 
 http.createServer(function (req, res) {
-  console.log("%s GET %s", new Date().getTime(), req.url);
+  console.log("%s %s GET %s", new Date().getTime(), getIp(req), req.url);
   var url_parts = url.parse(req.url, true);
   if (url_parts.pathname === '/favicon.ico') {
     res.writeHead(404);
@@ -23,7 +30,7 @@ http.createServer(function (req, res) {
   };
 
   if (!url_parts.query.latitude || !url_parts.query.longitude) {
-    res.writeHead(408, {'Content-Type': 'application/json'});
+    res.writeHead(400, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({
       "message": "Missing parameters: latitude, longitude."
     }));
