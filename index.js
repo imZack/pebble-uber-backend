c64cf1bebb3ceafe089ec4db5c1c579b11794614
YuLun Shih <shih@yulun.me>
2014-08-29 00:02:36 +0800

    Modulize

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var http = require('http');
-var url = require('url');
 var request = require('request');
 var qs = require('qs');
 
-var API_TOKEN = process.env.API_TOKEN;
-var IP = process.env.IP || "localhost";
-var PORT = process.env.PORT || 7777;
 
-function getIp(req) {
-  return req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress || 
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-}
+var API_TOKEN = process.env.API_TOKEN;
+
 
 if (!API_TOKEN) {
   console.error("API_TOKEN=your_token_here node %s", __filename);
@@ -20,7 +11,15 @@ if (!API_TOKEN) {
   process.exit(1);
 }
 
-function fetchSurgePrice(latitude, longitude, times, cb) {
+
+var getIp = function getIp(req) {
+  return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+};
+
+var fetchSurgePrice = function fetchSurgePrice(latitude, longitude, times, cb) {
   var params = {
     start_latitude: latitude,
     start_longitude: longitude,
@@ -37,7 +36,7 @@ function fetchSurgePrice(latitude, longitude, times, cb) {
         console.log("Can't fetch price for %s", request_url);
         console.log(body);
         cb(times);
-        return
+        return;
       }
       
       var prices = JSON.parse(body).prices;
@@ -51,9 +50,9 @@ function fetchSurgePrice(latitude, longitude, times, cb) {
       // finish, execute callback
       cb(times);
     });
-}
+};
 
-function fetchEstimateTime(latitude, longitude, cb) {
+var fetchEstimateTime = function fetchEstimateTime(latitude, longitude, cb) {
   var params = {
     server_token: API_TOKEN,
     start_latitude: latitude,
@@ -81,31 +80,10 @@ function fetchEstimateTime(latitude, longitude, cb) {
         cb(times);
       }
   });
-}
+};
 
-http.createServer(function (req, res) {
-  console.log("%s %s GET %s", new Date().getTime(), getIp(req), req.url);
-  var url_parts = url.parse(req.url, true);
-  if (url_parts.pathname === '/favicon.ico') {
-    res.writeHead(404);
-    res.end();
-    return;
-  };
-
-  if (!url_parts.query.latitude || !url_parts.query.longitude) {
-    res.writeHead(400, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-      "message": "Missing parameters: latitude, longitude."
-    }));
-    return;
-  };
-
-  fetchEstimateTime(url_parts.query.latitude, url_parts.query.longitude,
-    function(times) {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({times: times}))
-    }
-  );
-}).listen(PORT, IP);
-
-console.log('Server running at http://%s:%s', IP, PORT);
+module.exports = {
+  getIp: getIp,
+  fetchSurgePrice: fetchSurgePrice,
+  fetchEstimateTime: fetchEstimateTime
+};
